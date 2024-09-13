41bcd84f181b65789c1b7647c628b73cbb25c50f
gian_tiaga <ienakaev@ya.ru>
2024-09-13 11:27:23 +0300

    FactoryPlugin dev

--- resources/views/factory.blade.php
+++ resources/views/factory.blade.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @@extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\{{ $view->model() }}>
  */
-class RoleFactory extends Factory
+class {{ $view->model() }}Factory extends Factory
 {
     /**
      * Define the model's default state.
